4822bc6d36d757bdd93e403ea19a93b286c6dc7a
Alana Alexander <laninsky@gmail.com>
2018-03-23 14:24:59 +1300

    Update combining_samples_from_structure.R

--- combining_samples_from_structure.R
+++ combining_samples_from_structure.R
@@ -19,17 +19,20 @@ for (i in 1:length(structure_names)) {
   }
 }  
                     
-mod_structure <- as.tibble(orig_structure[-1,])
-names(mod_structure) <- structure_names
-          
-which(names(mod_structure) %in% c(sample1,sample2))                    
+mod_structure <- orig_structure[-1,]
+                    
+subsetcols <- which(structure_names %in% c(sample1,sample2))                    
+                    
+new_allele <- rep(NA,dim(mod_structure)[1])  
+new_allele[(which(mod_structure[,subsetcols[1]]!=mod_structure[,subsetcols[2]] & mod_structure[,subsetcols[1]]!=-9 & mod_structure[,subsetcols[2]]!=-9))] <- "-9"
+
+first_no_data <- which(mod_structure[,subsetcols[1]]==-9 & is.na(new_allele[]))
+new_allele[first_no_data] <- as.character(mod_structure[first_no_data,subsetcols[2]])
+second_no_data <- which(mod_structure[,subsetcols[2]]==-9 & is.na(new_allele[]))                    
+new_allele[second_no_data] <- as.character(mod_structure[second_no_data,subsetcols[1]])
                     
                     
-                    
-                    
-new_allele <- matrix(NA,ncol=1,nrow=dim(mod_structure)[2])                    
-new_allele                    
-                    
+is.na(new_allele[,1])                    
                     
                     
 newname <- paste(sample1,sample2,sep="_")
