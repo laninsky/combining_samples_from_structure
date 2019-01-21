ecba4be5be8bf87e7c358137b2cf4d0e9447f8d0
Alana Alexander <laninsky@gmail.com>
2019-01-21 15:31:20 +1300

    Update remove_lowest_coverage_sample_from_group.R

--- remove_lowest_coverage_sample_from_group.R
+++ remove_lowest_coverage_sample_from_group.R
@@ -1,24 +1,37 @@
-#working_dir <- "/Users/alanaalexander/Dropbox/lamprey"
-#structure_file <- "new_structure.stru"
-#sample1 <- "3.Lpry"
-#sample2 <- "13.Lpry"
-#missing <- "-9"
+working_dir <- "/Users/alanaalexander/Dropbox/lamprey/Allison_18Jan2019_structure/NZ"
+structure_file <- "NZ_ONLY_assembly.str"
+missing <- "-9"
+relatedness_file <- "related_individuals.txt"
 
-remove_lowest_coverage_sample_from_group <- function(working_dir,structure_file,missing,sample1,sample2) {
-
-library(tidyverse)
+remove_lowest_coverage_sample_from_group <- function(working_dir,structure_file,missing,relatedness_file) {
+  
+library(readr)
+library(dplyr)
 setwd(working_dir)
-orig_structure <- t(read.table(structure_file,header=FALSE))
-structure_names <- orig_structure[1,]
+  
+orig_structure <- readLines(structure_file)
 
-if (!(sample1 %in% structure_names)) {
-  stop(paste(sample1," does not exist in your structure file",sep=""))
+transposed_structure <- matrix(unlist(strsplit(orig_structure[1],"\t")),ncol=1)
+transposed_structurename <- gsub(" ","",transposed_structure[1])
+transposed_structure <- tibble(transposed_structure[-1])
+transposed_structure <- transposed_structure[-(which(transposed_structure=="")),1]
+names(transposed_structure) <- transposed_structurename
+
+for (i in 2:length(orig_structure)) {
+  temprow <- matrix(unlist(strsplit(orig_structure[i],"\t")),ncol=1)
+  temprowname <- gsub(" ","",temprow[1])
+  if (temprowname %in% names(transposed_structure)) {
+    names(transposed_structure)[which(names(transposed_structure)==temprowname)] <- paste(temprowname,"_A",sep="")
+    temprowname <- paste(temprowname,"_B",sep="")
+  }
+  temprow <- tibble(temprow[-1])
+  temprow <- temprow[-(which(temprow=="")),1]
+  names(temprow) <- temprowname
+  transposed_structure <- bind_cols(transposed_structure,temprow)
+  print(paste("Up to ",i," out of ",length(orig_structure),sep=""))
 }
-if (!(sample2 %in% structure_names)) {
-  stop(paste(sample2," does not exist in your structure file",sep=""))
-}
-                    
-mod_structure <- orig_structure[-1,]
+
+### UP TO HERE
 
 sample1cols <- which(structure_names %in% sample1)
 sample2cols <- which(structure_names %in% sample2)
